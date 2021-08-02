992b7b6d8ad2b24570570c59f3e19f6e7aa887e2
Micah Silverman <micah@afitnerd.com>
2021-08-02 16:21:55 -0400

    moved the main action to methods

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -31,9 +31,9 @@ public class DemoController {
     private String treatmentName;
 
     @GetMapping("/")
-    public String home(Principal principal) throws JsonProcessingException, InterruptedException {
+    public String home(Principal principal) throws InterruptedException, JsonProcessingException {
 
-        // start time for perofrmance tracking
+        // start time for performance tracking
         long startTime = System.nanoTime();
 
         // get the user name (you use this to retrieve the split treatment)
@@ -42,52 +42,18 @@ public class DemoController {
 
         // get the split treatment for the given authenticated user
         SplitResult result = splitClient.getTreatmentWithConfig(userName, treatmentName);
-        String treatment = result.treatment();
-
-        // our flag
-        boolean getPithySayingFromGitHub;
-
-        // set the boolean flag according to the treatment state
-        if (treatment.equals("on")) {
-            logger.info("Treatment " + treatmentName + " is ON");
-            getPithySayingFromGitHub = true;
-        } else if (treatment.equals("off")) {
-            getPithySayingFromGitHub = false;
-            logger.info("Treatment " + treatmentName + " is OFF");
-        } else {
-            throw new RuntimeException("Couldn't retrieve treatment from Split.");
-        }
 
         // get the millis delay value from the treatment configuration
-        long timeDelay = 0;
-        if (null != result.config()) {
-            TimeDelayConfig config = mapper.readValue(result.config(), TimeDelayConfig.class);
-            logger.info("timeDelay = " + config.timedelay);
-            timeDelay = config.timedelay;
-        }
-
-        // build the response based on the treatment values
-
-        // default response
-        String response = "Hey, " + userName;
-
-        // is this feature turned on in our treatment?
-        if (getPithySayingFromGitHub) {
-            String responseBody = WebClient
-                    .create("https://api.github.com/zen")
-                    .get()
-                    .retrieve()
-                    .bodyToMono(String.class)
-                    .block();
-
-            response = "Hey, " + userName + ", GitHub says: " + responseBody;
-        }
+        long timeDelay = getTimeDelay(result);
 
         // does the configuration specify a delay?
         if (timeDelay > 0) {
             logger.info("Threat.sleeping for " + timeDelay + " milliseconds");
             Thread.sleep(timeDelay);
         }
+
+        // build the response based on the treatment values
+        String response = getResponse(result, userName);
 
         // stop time for performance measuring
         long stopTime = System.nanoTime();
@@ -101,7 +67,37 @@ public class DemoController {
         splitClient.track(userName, "user", "home_method_time_millis", methodDurationMillis);
 
         return response;
+    }
 
+    private long getTimeDelay(SplitResult result) throws JsonProcessingException {
+        long timeDelay = 0;
+        if (null != result.config()) {
+            TimeDelayConfig config = mapper.readValue(result.config(), TimeDelayConfig.class);
+            logger.info("timeDelay = " + config.timedelay);
+            timeDelay = config.timedelay;
+        }
+        return timeDelay;
+    }
+
+    private String getResponse(SplitResult result, String userName) {
+        String treatment = result.treatment();
+        String response = "Hey, " + userName;
+        if ("on".equals(treatment)) {
+            logger.info("Treatment " + treatmentName + " is ON");
+            String responseBody = WebClient
+                    .create("https://api.github.com/zen")
+                    .get()
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .block();
+
+            response = "Hey, " + userName + ", GitHub says: " + responseBody;
+        } else if ("off".equals(treatment)) {
+            logger.info("Treatment " + treatmentName + " is OFF");
+        } else {
+            logger.debug("Unexpected treatment result: " + treatment);
+        }
+        return response;
     }
 
     // simple inner class for unpacking our treatment config
